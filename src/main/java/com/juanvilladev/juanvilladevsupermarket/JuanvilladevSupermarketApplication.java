1b4fba7189f5a79a24e549464012ddecd109d720
JUAND <juandavidvillaloaiza@utp.edu.co>
2022-02-28 00:44:37 -0500

    Se crean clases y con sus respectivos getters an setters

---
+++ src/main/java/com/juanvilladev/juanvilladevsupermarket/JuanvilladevSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.juanvilladev.juanvilladevsupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanvilladevSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanvilladevSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanvilladev/juanvilladevsupermarket/JuanvilladevSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanvilladev.juanvilladevsupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanvilladevSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
